6387a0518c648f0a34d438e3110e89364a14b896
iroziro <iroziro0526@gmail.com>
2017-02-09 13:44:14 +0900

    20170130 submit

---
+++ src/reservation/ChoiceFacility.java
@@ -0,0 +1,18 @@
+package reservation;
+
+import java.awt.Choice;
+
+public class ChoiceFacility extends Choice{
+ public ChoiceFacility(){
+	add("¬ƒz[ƒ‹");
+	add("‘å‰ï‹cº‚P");
+	add("‘å‰ï‹cº‚Q");
+	add("¬‰ï‹cº‚P");
+	add("¬‰ï‹cº‚Q");
+	add("¬‰ï‹cº‚R");
+	add("¬‰ï‹cº‚S");
+	add("¬‰ï‹cº‚T");
+	add("¬‰ï‹cº‚U");
+
+ }
+}
